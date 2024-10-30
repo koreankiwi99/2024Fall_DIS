79679272dae79754d43509fbf3025fd89e1b9b31
koreankiwi99 <salgu@snu.ac.kr>
2024-10-30 14:21:27 +0100

    Update and rename our_tf_idf.py to our_tfidf.py

--- our_tfidf.py
+++ our_tfidf.py
@@ -1,10 +1,10 @@
 import numpy as np
-from typing import List, Union
+from typing import List, Union, Optional, Dict
 from tqdm import tqdm
 import torch
 from collections import defaultdict, Counter
 
-class TF_IDF:
+class TfIdf:
   def __init__(self):
     self.word_set = set()
     self.d = 0 #number of word_set
@@ -18,12 +18,7 @@ class TF_IDF:
 
   def _split(self, doc)-> List[str]:
     return list(filter(self._filter, doc.split(' ')))
-
-  def _count_words(self, docs: List[List[str]]):
-    for doc in docs:
-      for word in doc:
-        self.word_count[word] += 1
-
+     
   #def tf(self, doc, word):
   #  return sum([token == word for token in doc]) / len(doc)
 
@@ -38,32 +33,45 @@ class TF_IDF:
     self.top_k = top_k
     self.total_n = len(corpus)
 
+    #local variables : only to save time when transforming
     docs_splitted = []
+    doc_word_freq = []
+
     for doc in corpus:
       splitted_doc = self._split(doc)
+      word_freq_doc = Counter(splitted_doc)
+      
       if self.top_k:
-        splitted_doc = [w for w, _ in Counter(splitted_doc).most_common(self.top_k)]
+        word_freq_doc = dict(word_freq_doc.most_common(self.top_k))
+        top_k_filter = lambda x : x in word_freq_doc
+        splitted_doc = list(filter(top_k_filter, splitted_doc))
 
-      self.word_set.update(splitted_doc)
+      for unique_word in word_freq_doc.keys():
+        self.word_count[unique_word] += 1 # 1 if word in doc else 0 (for IDF)
+        self.word_set.add(unique_word) #determine dimension
+
+      doc_word_freq.append(word_freq_doc)
       docs_splitted.append(splitted_doc)
 
     self.word_index = {word : idx for idx, word in enumerate(self.word_set)}
-    self._count_words(docs_splitted)
     self.d = len(self.word_set)
 
-    if return_matrix == True:
-      return self.transform(docs_splitted)
+    if return_matrix == True: #fit and transform
+      return self.transform(docs_splitted, doc_word_freq)
 
-  def transform(self, doc_list : List[List[str]]) -> np.array:
-    vec = np.zeros([len(doc_list),self.d], dtype=np.float32)
-    for idx, doc in enumerate(tqdm(doc_list)):
-      word_freq = Counter(doc)
-
+  def transform(self, 
+                doc_list : List[List[str]], 
+                doc_word_freq : Optional[Dict[str, int]]=None) -> np.array:
+    vec = np.zeros([len(doc_list),self.d], dtype=np.float32) # num of doc * dimension
+    for doc_idx, doc in enumerate(tqdm(doc_list)):
+      #if there is saved counter, use it
+      word_freq = doc_word_freq[doc_idx] if doc_word_freq != None else Counter(doc)
+      
       for word, count in word_freq.items():
         if word in self.word_index:
-          tf = count / len(doc) #to shorten time
-          idf = self.idf(word)
-          vec[idx, self.word_index[word]] = tf * idf
+          tf = count / len(doc) #TF(t, d) d-current doc / query
+          idf = self.idf(word) #IDF (t, D) D-whole corpus
+          vec[doc_idx, self.word_index[word]] = tf * idf
 
     return vec
 
@@ -74,8 +82,8 @@ class TF_IDF:
 
     for word, count in word_freq.items():
       if word in self.word_index:
-        tf = count / len(doc) #to shorten time
-        idf = self.idf(word)
+        tf = count / len(doc) #TF(t, d) d-current doc / query
+        idf = self.idf(word) #IDF (t, D) D-whole corpus
         vec[self.word_index[word]] = tf * idf
 
     return vec
