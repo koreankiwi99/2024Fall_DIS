83be17e3ceae130c9c188f9b43d8d32643e5b698
koreankiwi99 <salgu@snu.ac.kr>
2024-10-31 20:49:52 +0100

    Update our_bm25.py

--- our_bm25.py
+++ our_bm25.py
@@ -5,12 +5,12 @@ from tqdm import tqdm
 from operator import itemgetter
 
 class OurBM25:
-  def __init__(self, 
+  def __init__(self,
                k1=1.2,
-               b=0.75, 
+               b=0.75,
                epsilon=0.75,
                corpus_id : Optional[List[str]]=None,
-               top_k : Optional[int]=None, 
+               top_k : Optional[int]=500,
                round_decimals : int=2,
                save_form : str='slow'):
     self.k1 = k1
@@ -24,7 +24,7 @@ class OurBM25:
     #needs to be saved
     self.document_score = None
     self.word_index = dict()
-    
+
     #no need to save
     self.sum_doc_len, self.corpus_size = 0, 0  #for avg_doc_len
     self.avg_doc_len = 0
@@ -53,14 +53,10 @@ class OurBM25:
     average_idf = idf_sum / idf_len
     eps = self.epsilon * average_idf
     self.word2idf.update({word: eps for word in word_negative_idfs})
-    
-  def fit_transform(self, 
-                    corpus : List[str],
-                    top_k : int=None,
-                    round_decimals : int=2,
-                    save_form : str = 'slow'):
+
+  def fit_transform(self,
+                    corpus : List[str]):
     #local variables for transforming corpus
-    self.top_k = top_k
     doc_word_freq = []
     docs_splitted = []
 
@@ -84,23 +80,23 @@ class OurBM25:
       #save for transforming
       doc_word_freq.append(word_freq_doc)
       docs_splitted.append(splitted_doc)
-    
+
     self.avg_doc_len = self.sum_doc_len / self.corpus_size
     self._build_idf() #update word2idf
     self.word_index = {word: i for i, word in enumerate(self.word2idf.keys())}
     self.d = len(self.word2idf.keys())
-    
+
     #transform and save numpy array
     self._transform(docs_splitted, doc_word_freq)
-    
-  def _transform(self, 
+
+  def _transform(self,
                  docs_splitted : List[List[str]],
                  doc_word_freq : List[Dict[str, int]]):
     if self.save_form == 'fast':
       score_vecs = np.zeros([self.d, self.corpus_size], dtype=np.float32)
-            
+
     elif self.save_form == 'slow':
-      score_vecs = [defaultdict(int)] * self.corpus_size
+      score_vecs = [defaultdict(int) for _ in range(self.corpus_size)]
 
     for doc_idx, doc in enumerate(tqdm(docs_splitted)):
       doc_freqs = doc_word_freq[doc_idx]
@@ -108,34 +104,41 @@ class OurBM25:
       for word in doc_freqs.keys(): #use as word set
         x = self.word2idf[word] * doc_freqs[word] * (self.k1 + 1)
         y = doc_freqs[word] + self.k1 * (1 - self.b + self.b * len(doc) / self.avg_doc_len)
-        score = round((x / y) + 1, self.round_decimals) #todo
-      
+        score = np.round((x / y) + 1, self.round_decimals) #todo
+
         if self.save_form == 'fast':#loading slow, fast retrieval
           score_vecs[self.word_index[word], doc_idx] = score
 
         elif self.save_form == 'slow':#loading fast, slow retrieval
           score_vecs[doc_idx][self.word_index[word]] = score
-    
+
     self.document_score = score_vecs
 
-  def search(self, 
-             query : Union[List[str], str], 
-             k : int=10, 
+  def search(self,
+             query : Union[List[str], str],
+             k : int=10,
              return_score : bool=True):
     splitted_query = self._split(query) if type(query) == str else query
+    word_indices = [self.word_index[w] for w in splitted_query if w in self.word_index] # num_word * num_docs
+    if len(word_indices) == 0:#not a single word in the query matching for word_set
+      return None
+
     if self.save_form == 'fast': #loading slow, fast retrieval
-      word_indices = [self.word_index[w] for w in splitted_query if w in self.word_index] # num_word * num_docs 
       scores = sum(self.document_score[word_indices, :]) # 1 * num_docs
       top_doc_k = np.argsort(scores)[::-1][:k]
-    
+
     elif self.save_form == 'slow':#loading fast, slow retrieval
-      word_indices = [self.word_index[w] for w in splitted_query if w in self.word_index] # num_word * num_docs 
-      get_word_scores = lambda doc : sum(itemgetter(*word_indices)(doc))
+      if len(word_indices) == 1:
+        get_word_scores = lambda doc : doc[word_indices[0]]
+
+      else:
+        get_word_scores = lambda doc : sum(list(itemgetter(*word_indices)(doc)))
+
       scores = np.array([get_word_scores(doc) for doc in self.document_score])
       top_doc_k = np.argsort(scores)[::-1][:k]
 
     if return_score == False:
       return self.corpus_id[top_doc_k] if self.corpus_id else top_doc_k
-    
+
     else:
       return [(scores[idx], idx) for idx in top_doc_k]
