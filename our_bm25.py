034b2e928418a2740e11139b87422f32801b8c71
koreankiwi99 <salgu@snu.ac.kr>
2024-11-01 18:27:41 +0100

    Update our_bm25.py

--- our_bm25.py
+++ our_bm25.py
@@ -96,7 +96,7 @@ class OurBM25:
       score_vecs = np.zeros([self.d, self.corpus_size], dtype=np.float32)
 
     elif self.save_form == 'economic':
-      score_vecs = [[0 for _ in range(self.d)] for _ in range(self.corpus_size)]
+      score_vecs = [dict() for _ in range(self.corpus_size)]
 
     elif self.save_form == 'slow':
       score_vecs = [defaultdict(int) for _ in range(self.corpus_size)]
